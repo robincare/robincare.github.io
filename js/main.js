3d7d3d5b04edaf0bc106d7be1b7bb2741515da08
Sam Purcell <samuel.f.purcell@gmail.com>
2016-11-29 18:48:06 -0500

    Add font-files/images and update main index page

---
+++ js/main.js
@@ -0,0 +1,30 @@
+  $( function() {
+    function hexFromRGB(r, g, b) {
+      var hex = [
+        r.toString( 16 ),
+        g.toString( 16 ),
+        b.toString( 16 )
+      ];
+      $.each( hex, function( nr, val ) {
+        if ( val.length === 1 ) {
+          hex[ nr ] = "0" + val;
+        }
+      });
+      return hex.join( "" ).toUpperCase();
+    }
+    function refreshSwatch() {
+      var red = $( "#progress-bar" ).slider( "value" ),
+        hex = hexFromRGB( red, green, blue );
+      $( "#swatch" ).css( "background-color", "#" + hex );
+    }
+
+    $( "#progress-bar").slider({
+      orientation: "horizontal",
+      range: "min",
+      max: 255,
+      value: 127,
+      slide: refreshSwatch,
+      change: refreshSwatch
+    });
+    $( "#progress-bar" ).slider( "value", 255 );
+  } );
